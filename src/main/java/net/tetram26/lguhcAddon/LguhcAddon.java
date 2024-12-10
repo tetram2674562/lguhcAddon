daaee82470697d7c189bd59595edf611c0e5a064
tetram2674562 <aymericgab77@gmail.com>
2024-12-10 18:48:41 +0100

    Added spectator mode.

--- src/main/java/net/tetram26/lguhcAddon/LguhcAddon.java
+++ src/main/java/net/tetram26/lguhcAddon/LguhcAddon.java
@@ -1,13 +1,16 @@
 package net.tetram26.lguhcAddon;
 
+import fr.ph1lou.werewolfapi.GetWereWolfAPI;
+import net.tetram26.lguhcAddon.listeners.JoiningListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LguhcAddon extends JavaPlugin {
-
+    public static GetWereWolfAPI ww;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        GetWereWolfAPI ww = getServer().getServicesManager().load(GetWereWolfAPI.class);
+        getServer().getPluginManager().registerEvents(new JoiningListener(), this);
     }
 
     @Override
--- src/main/java/net/tetram26/lguhcAddon/listeners/JoiningListener.java
+++ src/main/java/net/tetram26/lguhcAddon/listeners/JoiningListener.java
@@ -1,4 +1,21 @@
 package net.tetram26.lguhcAddon.listeners;
 
-public class JoiningListener {
+import fr.ph1lou.werewolfapi.enums.StateGame;
+import org.bukkit.GameMode;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+
+import static net.tetram26.lguhcAddon.LguhcAddon.ww;
+
+public class JoiningListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onClientConnected(PlayerJoinEvent event){
+        if (ww.getWereWolfAPI().isState(StateGame.GAME)) {
+            event.getPlayer().setGameMode(GameMode.SPECTATOR);
+        }
+    }
 }
