2d5751e6a3d603aa1669fbccb067845b75ee8718
ht06 <hthug06@gmail.com>
2024-12-10 13:38:00 +0100

    Ajout de la commande de rtp et de celle pour reload la config.

--- src/main/java/net/tetram26/lguhcAddon/LguhcAddon.java
+++ src/main/java/net/tetram26/lguhcAddon/LguhcAddon.java
@@ -1,17 +1,34 @@
 package net.tetram26.lguhcAddon;
 
+import fr.ph1lou.werewolfapi.GetWereWolfAPI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class LguhcAddon extends JavaPlugin {
 
+    public static GetWereWolfAPI ww = getInstance().getServer().getServicesManager().load(GetWereWolfAPI.class);
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
+        getCommand("randomtp").setExecutor(new RtpCommand());
+        getCommand("lguhcAddon").setExecutor(this);
+        getCommand("randomtp").setTabCompleter(new RtpCommand());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    public static LguhcAddon getInstance(){
+        return getPlugin(LguhcAddon.class);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        reloadConfig();
+        return true;
     }
 }
